fa47f20f0da3aa78677d15a6ac18888cc455b937
wabathur <safbul@gmail.com>
2022-10-18 14:51:26 +0300

    ex03 done

--- ex02/src/Program.java
+++ ex02/src/Program.java
@@ -8,7 +8,13 @@ public class Program {
         }
         System.out.println("balance of user[19] is " + arrayList.getUserByIndex(19).getBalance());
         System.out.println("number of users is " + arrayList.getNumberOfUsers());
-        arrayList.getUserById(123);
-        arrayList.getUserByIndex(-1);
+        try {
+            arrayList.getUserById(123);
+        }
+        catch (UserNotFoundException e){}
+        try {
+            arrayList.getUserById(-1);
+        }
+        catch (UserNotFoundException e){}
     }
 }
